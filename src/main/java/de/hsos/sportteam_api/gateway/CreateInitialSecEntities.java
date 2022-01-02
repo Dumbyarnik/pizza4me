bfc397e20219347bb4f55c2428d1d9c7f9667611
Dumbyarnik <62794996+Dumbyarnik@users.noreply.github.com>
2022-01-02 16:18:18 +0100

    Creating user by default

--- src/main/java/de/hsos/sportteam_api/gateway/CreateInitialSecEntities.java
+++ src/main/java/de/hsos/sportteam_api/gateway/CreateInitialSecEntities.java
@@ -1,5 +1,6 @@
 package de.hsos.sportteam_api.gateway;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.enterprise.event.Observes;
 import javax.transaction.Transactional;
@@ -7,11 +8,16 @@ import javax.transaction.Transactional;
 import de.hsos.sportteam_api.entities.security.UserLogin;
 import io.quarkus.runtime.StartupEvent;
 
+
+
 @Singleton
 public class CreateInitialSecEntities {
+    @Inject
+    KundenRepository kundenRepository;
+
     @Transactional
     public void loadUsers(@Observes StartupEvent event){
         UserLogin.deleteAll();
-        UserLogin.add("user", "user", "KundIn");
+        kundenRepository.createKunde("user", "user");
     }
 }
